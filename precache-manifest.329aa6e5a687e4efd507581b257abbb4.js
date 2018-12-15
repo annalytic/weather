5c2894735f1393e72b9e7056ce833eb889120f49
Anna Li <halloannali@gmail.com>
2018-12-15 14:20:49 +0100

    Updates

---
+++ precache-manifest.329aa6e5a687e4efd507581b257abbb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "4b7607741f22dfa92e62",
+    "url": "/weather/static/js/main.4b760774.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/weather/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "4b7607741f22dfa92e62",
+    "url": "/weather/static/css/main.670ba107.chunk.css"
+  },
+  {
+    "revision": "5724b7444c90156af6ee2d89d8ce64d9",
+    "url": "/weather/index.html"
+  }
+];
